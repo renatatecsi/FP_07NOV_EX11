5f32d31ca18df752d09e890df5abb95582b684fd
renata <renata@DESKTOP-7K85QAI>
2022-11-20 10:14:32 +0200

    Add project files.

---
+++ FP_07NOV_EX11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace continue2
+{
+    class Progarm
+    {
+        static void Main(string[] args)
+        {
+            int n, x;
+            Console.WriteLine("Dati un numar:");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("x =");
+            x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; ++i)
+            {
+                if (i % x == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
